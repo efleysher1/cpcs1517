f060c353c832cbcea5924b9876388fd0e6cbfb34
efleysher1 <46543444+efleysher1@users.noreply.github.com>
2019-03-25 13:53:36 -0600

    Fix

--- CSNet/NorthwindSystem/DAL/NorthwindContext.cs
+++ CSNet/NorthwindSystem/DAL/NorthwindContext.cs
@@ -37,6 +37,5 @@ namespace NorthwindSystem.DAL
         // <T> is your class in the .Data project
         //this is a property
         public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
     }
 }
