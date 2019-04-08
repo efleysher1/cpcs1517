096ab70270daded9b0e7eb6cdd3f8e729d55ba61
efleysher1 <46543444+efleysher1@users.noreply.github.com>
2019-04-08 16:20:57 -0600

    Set Up Object Data Source

--- CSNet/NorthwindSystem/BLL/CategoryController.cs
+++ CSNet/NorthwindSystem/BLL/CategoryController.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using NorthwindSystem.Data; //obtains the <T> definitions
 using NorthwindSystem.DAL;  //obtains the context class
+using System.ComponentModel; //to expose a class/methods to the ODS dialogs
 #endregion
 
 namespace NorthwindSystem.BLL
 {
+    //expose the controller to the ODS implementation dialogs
+    [DataObject]
     public class CategoryController
     {
         public Category Category_Get(int categoryid)
@@ -21,6 +24,8 @@ namespace NorthwindSystem.BLL
             }
         }
 
+        //expose a specific method to the ODS implementation dialogs
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<Category> Category_List()
         {
             using (var context = new NorthwindContext())
